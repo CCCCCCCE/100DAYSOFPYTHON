f497bcd60977e1d2382ab33534cd2aba17e544f2
Einstein30 <shindesmita650@gmail.com>
2022-05-14 23:48:05 +0530

    1

---
+++ DAY1/BandNameGen.py
@@ -0,0 +1,4 @@
+print("Welcomw to the Band Name Generator.")
+name = input("What's name of the city you grew up in?")
+petname = input("What's your pet's name?")
+print("Your band name could be " +name + petname)
