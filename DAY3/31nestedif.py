23e4e5c6a9f20696fcc1d3d038279b6d9efc689a
Smita Shinde <shindesmita650@gmail.com>
2022-06-16 22:07:13 +0530

    Update 31nestedif.py

--- DAY3/31nestedif.py
+++ DAY3/31nestedif.py
@@ -2,16 +2,12 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-
 if height >= 120:
-    
     print("You Can ride the Rollercoaster!")
     age = int(input("How old are you? "))
     if age >= 18:
         print("You have to pay $12 to take ride of rollercoster")
     else:
-        print("You have to pay $7 to take ride of rollercoster")
-
-  
+        print("You have to pay $7 to take ride of rollercoster")  
 else:
   print("Sorry ,You have to grow taller before You Can ride!")
