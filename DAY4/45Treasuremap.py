e257334791f084f856d58d4d5259f281eb13faaa
Smita Shinde <shindesmita650@gmail.com>
2022-06-16 22:45:03 +0530

    Update 45Treasuremap.py

--- DAY4/45Treasuremap.py
+++ DAY4/45Treasuremap.py
@@ -8,13 +8,9 @@ position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-
-
 horizontal = int(position[0])
 vertical = int(position[1])
-
 map[vertical - 1][horizontal - 1] = "X"
-
 
 #Write your code above this row 👆
 
